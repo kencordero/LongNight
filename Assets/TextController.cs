9ed3bc2e769793606d296724470f8b7f736254d0
Kenneth Cordero <kencordero@gmail.com>
2016-03-17 23:51:27 -0500

    Merge old changes

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -1,12 +1,10 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
-using System;
 
 public class TextController : MonoBehaviour {
 
-    public Text text;
-    private enum GameStates { Awoken, Baby, Glasses, Sleep, Start, Time, PickUpBaby };
+    public Text body;
+    private enum GameStates { Awoken, Baby, Glasses, Sleep, Start, Time, PickUpBaby, LeaveNursery, Wait };
     private GameStates gameState;
     private bool hasGlasses;
 
@@ -21,44 +19,46 @@ public class TextController : MonoBehaviour {
         switch (gameState)
         {
             case GameStates.Awoken:
-                text.text = "You awaken to a loud shriek. You know not what time it is - it's still pitch black " +
+                body.text = "You awaken to a loud shriek. You know not what time it is - it's still pitch black " +
                     "outside - but you have a pretty good idea where the sound is coming from.\n\n";
                 displayOptions(new []{GameStates.Time, GameStates.Baby, GameStates.Sleep});
                 break;
             case GameStates.Glasses:
-                text.text = "You scrabble around on the night stand, but your glasses do not appear\n\n";
+                body.text = "You scrabble around on the night stand, but your glasses do not appear\n\n";
                 displayOptions(new []{GameStates.Sleep});
                 break;
             case GameStates.Sleep: 
-                text.text = "Who are you kidding?!?\n\n";
+                body.text = "Who are you kidding?!?\n\n";
 				displayOptions(new []{GameStates.Time, GameStates.Baby});
                 break;
             case GameStates.Start:
-                text.text = "Welcome to The Long Night!\n\n";
+                body.text = "Welcome to The Long Night!\n\n";
                 displayOptions(new []{GameStates.Awoken});
                 break;        
             case GameStates.Time:
                 if (hasGlasses)
                 {
-                    text.text = "You look over at the clock and see that it is really late. It's so late, it's " +
+                    body.text = "You look over at the clock and see that it is really late. It's so late, it's " +
                         "actually rather early.\n\n";
                     displayOptions(new []{GameStates.Sleep});
                 }
                 else
                 {
-                    text.text = "You don't have your glasses so you can't see the clock across " +
+                    body.text = "You don't have your glasses so you can't see the clock across " +
                         "the room. Why you keep the clock on the other side of the room is a " +
                         "question for another sleepless night.\n\n";
                     displayOptions(new []{GameStates.Sleep});
                 }
                 break;
             case GameStates.Baby:
-                text.text = "You enter your infant daughter's nursery room and she's suddenly quiet and still. " +
-                    "Her eyes are fixed on you but she doesn't seem to be perturbed.\n\n";
-                displayOptions(new []{GameStates.PickUpBaby});
+                body.text = "You enter your infant daughter's nursery room and she's suddenly quiet and still. " +
+                    "Her eyes are fixed on you and she no longer seems to be perturbed.\n\n";
+                displayOptions(new []{GameStates.PickUpBaby, GameStates.LeaveNursery, GameStates.Wait});
                 break;
             default:
-                throw new NotImplementedException(gameState.ToString());
+                body.text = "DEAD END!\n";
+                displayOptions(new []{GameStates.Awoken});
+                break;
         }
 	}
     
@@ -69,31 +69,40 @@ public class TextController : MonoBehaviour {
             switch(option)
             {
                 case GameStates.Awoken:
-                    text.text += "Press Space key to Start a New Game\n";
+                    body.text += "Press the Space key to Start a New Game\n";
                     if (Input.GetKeyDown(KeyCode.Space)) gameState = GameStates.Awoken;
                     break;
                 case GameStates.Baby:
-                    text.text += "Press [B] to check on the Baby\n";
+                    body.text += "Press [B] to check on the Baby\n";
                     if (Input.GetKeyDown(KeyCode.B)) gameState = GameStates.Baby;
-                    break;
+                    break;                
                 case GameStates.Glasses:
-                    text.text += "Press [G] to look for your Glasses\n";
+                    body.text += "Press [G] to look for your Glasses\n";
                     if (Input.GetKeyDown(KeyCode.G)) gameState = GameStates.Glasses;
                     break;
+                case GameStates.LeaveNursery:
+                    body.text += "Press [L] to Leave the nursery.\n";
+                    if (Input.GetKeyDown(KeyCode.L)) gameState = GameStates.LeaveNursery;
+                    break;
                 case GameStates.PickUpBaby:
-                    text.text += "Press [P] to Pick up the baby.\n";
+                    body.text += "Press [P] to Pick up the baby.\n";
                     if (Input.GetKeyDown(KeyCode.P)) gameState = GameStates.PickUpBaby;
                     break;
                 case GameStates.Sleep:
-                    text.text += "Press [S] to go to Sleep.\n";
+                    body.text += "Press [S] to go to Sleep.\n";
                     if (Input.GetKeyDown(KeyCode.S)) gameState = GameStates.Sleep;
                     break;
                 case GameStates.Time:
-                    text.text += "Press [T] to look at the Time\n";
+                    body.text += "Press [T] to look at the Time\n";
                     if (Input.GetKeyDown(KeyCode.T)) gameState = GameStates.Time;
+                    break;
+                case GameStates.Wait:
+                    body.text += "Press [W] to Wait there.\n";
+                    if (Input.GetKeyDown(KeyCode.W)) gameState = GameStates.Wait;
                     break;                                                                               
                 default:
-                    throw new NotImplementedException(gameState.ToString());
+                    body.text += "NO OPTION SET UP FOR " + gameState.ToString();
+                    break;
             }
         }
     }
